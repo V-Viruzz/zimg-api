6e676f607e0ea679ebfa28f0e0f32e4d1415e9b1
viruz <nahuelsinka@gmail.com>
2023-04-09 17:11:34 -0300

    type module

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const mycors = require('cors')
-const multer = require('multer')
-const sharp = require('sharp')
-const path = require('path')
+import express, { urlencoded, json } from 'express'
+import mycors from 'cors'
+import multer from 'multer'
+import sharp from 'sharp'
 
 const upload = multer()
 const app = express()
@@ -12,7 +11,7 @@ const URL = 'https://zimg-x.vercel.app'
 const db = []
 
 app.use(
-  express.urlencoded({
+  urlencoded({
     extended: true
   })
 )
@@ -31,7 +30,6 @@ app.use(
 
 // Cuando te hagan un post http://localhost:3000/transactions
 app.post('/upload', upload.single('file'), (req, res) => {
-  // res.set('Access-Control-Allow-Origin', 'http://localhost:4000')
   const file = req.file
   db.push(file)
   console.log(file)
@@ -47,20 +45,11 @@ app.post('/upload', upload.single('file'), (req, res) => {
   })
 })
 
-app.post('/props', express.json(), (req, res) => {
+app.post('/props', json(), (req, res) => {
   const props = req.body
 
-  const pathRes = `./tmp/${props.filename}`
-  const pathSave = `./image/${props.filename}.${props.format}`
-  const pathImage = path.join(__dirname, pathSave)
   const resolution = { width: props.width, height: props.height }
-
   const image = db.find(item => item.originalname === props.filename)
-
-  // fs.readFile(pathRes, (err, buffer) => {
-  //   if (err) {
-  //     console.error(err)
-  //   }
 
   // Hacer lo que necesites con el buffer de la imagen
   sharp(image.buffer, { animated: props.format === 'gif' || props.format === 'webp' })
@@ -71,24 +60,8 @@ app.post('/props', express.json(), (req, res) => {
 
       res.send(data)
     })
-    // .toFile(pathSave)
-    // .then(() => res.sendFile(pathImage)
-
-  // })
 })
 
 app.listen(port, () => {
   console.log(`Estoy ejecutandome en http://localhost:${port}`)
 })
-
-// setInterval(() => {
-//   console.log('cleanup')
-//   db.forEach((item) => {
-//     const pathTemp = path.join(__dirname, `./temp/${item.filename}`)
-//     fs.unlink(pathTemp, (err) => {
-//       if (err) {
-//         console.error('Error al eliminar archivo:')
-//       }
-//     })
-//   })
-// }, 10 * 60 * 1000)
